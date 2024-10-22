2ceadc502e1482b573330e155748626769c5020e
Siddhanta Acharya <siddhu12980@gmail.com>
2024-10-23 03:48:02 +0530

    added go controller

--- go_engine/engine.go
+++ go_engine/engine.go
@@ -14,28 +14,28 @@ func HandleRes(res string, err error) {
 
 func main() {
 
-	res, err := controller.GetINRBalance("user122")
+	// res, err := controller.GetINRBalance("user122")
 
-	HandleRes(res, err)
+	// HandleRes(res, err)
 
-	fmt.Printf("\n")
+	// fmt.Printf("\n")
 
-	res2, err2 := controller.GetStockBalance("user1")
+	// res2, err2 := controller.GetStockBalance("user1")
 
-	HandleRes(res2, err2)
+	// HandleRes(res2, err2)
 
-	fmt.Printf("\n")
-	res3, err3 := controller.GetAllINRBalance()
+	// fmt.Printf("\n")
+	// res3, err3 := controller.GetAllINRBalance()
 
-	HandleRes(res3, err3)
+	// HandleRes(res3, err3)
 
-	fmt.Printf("\n")
+	// fmt.Printf("\n")
 
-	res4, err4 := controller.GetAllStockBalance()
+	// res4, err4 := controller.GetAllStockBalance()
 
-	HandleRes(res4, err4)
+	// HandleRes(res4, err4)
 
-	fmt.Printf("\n \n")
+	// fmt.Printf("\n \n")
 
 	res5, err5 := controller.CreateSymbol("NEW")
 
@@ -49,8 +49,10 @@ func main() {
 
 	fmt.Printf("\n \n")
 
-	res7, err7 := controller.CreateSymbol("NEW")
+	res7, err7 := controller.GetAllOrderbook()
 
-	HandleRes(res5, err5)
+	fmt.Printf("\n \n")
+
+	HandleRes(res7, err7)
 
 }
